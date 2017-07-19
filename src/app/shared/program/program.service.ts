b01238d42db7f9802cfedfb13a981a93e2a066ab
vreyvrey <vincent.rey@oprasteria.com>
2017-07-19 17:16:36 +0200

    Exo observables

--- src/app/shared/program/program.service.ts
+++ src/app/shared/program/program.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Program } from './program';
+import {Http, RequestOptions} from "@angular/http";
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 
 @Injectable()
 export class ProgramService {
 
-  constructor() { }
+  constructor(private http:  Http) {
+    this.http.get('/api/programs').subscribe(test => console.log(test.json()));// map(res => res.json())
+    this.http.get('/api/programs').filter(res => res.status === 200).subscribe(test => console.log(test.json()));// map(res => res.json())
+    this.http.get('/api/programs').map(res => {// map : transformation sur ce qu'on reçoit
+      res.status = 201;
+      return res;
+    }).filter(res => res.status === 200).subscribe(test => console.log(test.json()));
+    this.http.get('/api/programs').
+      do(res => console.log(res.status)).
+      map(res => {// map : transformation sur ce qu'on reçoit
+      res.status = 201;
+      return res;
+    })
+      .filter(res => res.status === 200).subscribe(test => console.log(test.json()));
+  }
 
   /**
    * voir la page /plan, il doit y avoir 3 éléments
    * @returns {null}
    */
   findAll(): Observable<Program[]> {
-    return null;
+    return this.http.get('/api/programs').map(res => res.json());
   }
 
   /**
@@ -20,7 +38,10 @@ export class ProgramService {
    * @returns {null}
    */
   findAllPlusEmpty(): Observable<Program[]> {
-    return null;
+    return this.findAll().map(programs => {
+      programs.push({name: 'Test'});
+      return programs;
+    });
   }
 
   /**
@@ -29,7 +50,7 @@ export class ProgramService {
    * @returns {null}
    */
   getProgramByIndex(index: number): Observable<any> {
-    return null;
+    return this.http.get(`/api/programs/${index}`).map(program => program.json());
   }
 
   /**
@@ -38,6 +59,7 @@ export class ProgramService {
    * @param program
    */
   updateProgram(index: number, program: Program): void {
+    this.http.post(`/api/programs/${index}`, program).subscribe(response => response.json());// subscribe nécessaire car méthode renvoie void (personne n'y souscrit côté IHM
   }
 
   /**
@@ -45,6 +67,7 @@ export class ProgramService {
    * @param program
    */
   addProgram(program: Program): void {
+    this.http.post('/api/programs', program).subscribe(response => response.json());// subscribe nécessaire car méthode renvoie void (personne n'y souscrit côté IHM
   }
 
   /**
@@ -52,5 +75,6 @@ export class ProgramService {
    * @param index
    */
   deleteProgram(index: number): void {
+    this.http.delete(`/api/programs/${index}`).subscribe(response => response.json());
   }
 }
